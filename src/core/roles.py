6548a51c8475e493a71acb697100710a0d3c2529
marko-miletic <marko.g.miletic@gmail.com>
2022-12-13 14:41:37 +0100

    feat: add user role permissions

---
+++ src/core/roles.py
@@ -0,0 +1,30 @@
+from functools import wraps
+from flask import redirect, url_for
+from flask_login import current_user
+
+
+def role_required(role_value: int, redirect_endpoint: str = 'auth.login'):
+    def decorator(func):
+        @wraps(func)
+        def authorize(*args, **kwargs):
+
+            # admin user pass for all roles
+            if current_user.role == 2:
+                return func(*args, **kwargs)
+
+            # roles permission check for non-admin roles
+            if not current_user.role == role_value:
+                return redirect(url_for(redirect_endpoint))
+            return func(*args, **kwargs)
+
+        return authorize
+    return decorator
+
+
+def role_names(role: str) -> int:
+    roles = {
+        'admin': 2,
+        'moderator': 1,
+        'user': 0
+    }
+    return roles.get(role, 0)
